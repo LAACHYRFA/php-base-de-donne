fbed98afd13f8516cea459f007ddd7a56a0addd7
LAACHYRFA <laachyr.farah.solicode@gmail.com>
2026-02-24 10:01:53 +0000

    Add files via upload

---
+++ Chapitre 3 – Insérer, mettre à jour et supprimer des enregistrements/connecion.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';      
+$dbname = 'proget_bdd'; 
+$username = 'root';      
+$password = '';          
+
+try {
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+    exit; 
+}
+?>
